1aec127e7c4d87e75491e1164811fcf175c67006
Clément Bouillier <clement.bouillier@devcrafting.com>
2018-02-28 09:26:50 +0100

    transform GameState into class to prepare for State Machine impl

--- csharp2/Game.cs
+++ csharp2/Game.cs
@@ -4,73 +4,25 @@ namespace TennisGame.Tests
 {
     internal class Game
     {
-        public Score Player1Score { get; private set; }
-        public Score Player2Score { get; private set; }
-        
-        private GameState State
-        {
-            get
-            {
-                if (Player1Score == Score.Forty && Player2Score == Score.Forty)
-                    return GameState.Deuce;
-                if (Player1Score == Score.Advantage || Player2Score == Score.Advantage)
-                    return GameState.Advantage;
-                return GameState.PointScore;
-            }
-        }
+        private GameState _gameState;
+
+        public Score Player1Score => _gameState.Player1Score;
+        public Score Player2Score => _gameState.Player2Score;
 
         public Game()
         {
+            _gameState = new GameState(Score.Love, Score.Love);
         }
 
         public Game(Score player1Score, Score player2Score)
         {
-            this.Player1Score = player1Score;
-            this.Player2Score = player2Score;
+            _gameState = new GameState(player1Score, player2Score);
         }
 
         internal Game ScoreAPoint(Player player)
         {
-            if (State == GameState.PointScore)
-            {
-                if (player == Player.Player1)
-                    Player1Score = AddPoint(Player1Score);
-                else
-                    Player2Score = AddPoint(Player2Score);
-            }
-            else if (State == GameState.Deuce)
-            {
-                if (player == Player.Player1)
-                    Player1Score = Score.Advantage;
-                else
-                    Player2Score = Score.Advantage;
-            }
-            else if (State == GameState.Advantage)
-            {
-                if (player == Player.Player1)
-                    if (Player2Score == Score.Advantage)
-                        Player2Score = Score.Forty;
-                    else
-                        Player1Score = Score.Game;
-                else
-                    if (Player2Score == Score.Advantage)
-                    Player2Score = Score.Game;
-                else
-                    Player1Score = Score.Forty;
-            }
+            _gameState = _gameState.ScoreAPoint(player);
             return this;
-        }
-
-        private Score AddPoint(Score playerScore)
-        {
-            if (playerScore == Score.Love)
-                return Score.Fifteen;
-            else if (playerScore == Score.Fifteen)
-                return Score.Thirty;
-            else if (playerScore == Score.Thirty)
-                return Score.Forty;
-            else
-                return Score.Game;
         }
     }
 }
--- csharp2/GameState.cs
+++ csharp2/GameState.cs
@@ -1,10 +1,78 @@
 namespace TennisGame.Tests
 {
-    public enum GameState
+    internal class GameState
     {
-        PointScore,
-        Deuce,
-        Advantage,
-        Game
+        public GameState(Score player1Score, Score player2Score)
+        {
+            Player1Score = player1Score;
+            Player2Score = player2Score;
+        }
+
+        public Score Player1Score { get; private set; }
+        public Score Player2Score { get; private set; }
+        
+        private StateEnum State
+        {
+            get
+            {
+                if (Player1Score == Score.Forty && Player2Score == Score.Forty)
+                    return StateEnum.Deuce;
+                if (Player1Score == Score.Advantage || Player2Score == Score.Advantage)
+                    return StateEnum.Advantage;
+                return StateEnum.PointScore;
+            }
+        }
+
+        internal GameState ScoreAPoint(Player player)
+        {
+            if (State == StateEnum.PointScore)
+            {
+                if (player == Player.Player1)
+                    Player1Score = AddPoint(Player1Score);
+                else
+                    Player2Score = AddPoint(Player2Score);
+            }
+            else if (State == StateEnum.Deuce)
+            {
+                if (player == Player.Player1)
+                    Player1Score = Score.Advantage;
+                else
+                    Player2Score = Score.Advantage;
+            }
+            else if (State == StateEnum.Advantage)
+            {
+                if (player == Player.Player1)
+                    if (Player2Score == Score.Advantage)
+                        Player2Score = Score.Forty;
+                    else
+                        Player1Score = Score.Game;
+                else
+                    if (Player2Score == Score.Advantage)
+                    Player2Score = Score.Game;
+                else
+                    Player1Score = Score.Forty;
+            }
+            return this;
+        }
+
+        private Score AddPoint(Score playerScore)
+        {
+            if (playerScore == Score.Love)
+                return Score.Fifteen;
+            else if (playerScore == Score.Fifteen)
+                return Score.Thirty;
+            else if (playerScore == Score.Thirty)
+                return Score.Forty;
+            else
+                return Score.Game;
+        }
+
+        private enum StateEnum
+        {
+            PointScore,
+            Deuce,
+            Advantage,
+            Game
+        }
     }
 }
