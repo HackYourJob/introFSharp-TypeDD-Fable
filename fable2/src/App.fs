dac6dcd3de2f9b64c0ba135591774731124dd45b
fpellet <git@florentpellet.com>
2019-05-23 13:47:47 +0200

    Clean fable

--- fable2/src/App.fs
+++ fable2/src/App.fs
@@ -1,16 +1,10 @@
 module App.Bootstrap
 
-(**
- The famous Increment/Decrement ported from Elm.
- You can find more info about Elmish architecture and samples at https://elmish.github.io/
-*)
-
 open Elmish
 open Elmish.React
 open App.Update
 open App.View
 
-// App
 Program.mkSimple init update view
 |> Program.withReact "elmish-app"
 |> Program.withConsoleTrace
